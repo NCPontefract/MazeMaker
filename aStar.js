65bd7581c77ffd4311f71d9aeca07a7bcb36147f
Ytrewq13 <sam.everythingcomputers@gmail.com>
2016-11-01 09:08:30 +0000

    Update aStar.js

--- aStar.js
+++ aStar.js
@@ -1,1 +1,15 @@
+// This is where I will implement the A* search algorithm in order to optimise finding a shortest solution.
 
+var openList = [];
+var closedList = [];
+
+// Function to add the first cell to the closed list.
+function startPath(cell) {
+  closedList = [];
+  cell.gCost = 0;
+  cell.searched = true;
+  closedList.push(cell);
+}
+function setGcost(cell) {
+  cell.gCost = grid[cell.Parent].gCost + 1;
+}
