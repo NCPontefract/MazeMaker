e52a6e609ee3b47ce6ef5f3eaf50757148c6b101
Ytrewq13 <sam.everythingcomputers@gmail.com>
2016-11-01 13:16:46 +0000

    Update aStar.js

--- aStar.js
+++ aStar.js
@@ -97,7 +97,7 @@ function solveLoop(start, goal) {
   closedList.push(nextCell);
   getChildren(nextCell);
   if (!contains(closedList, goal)) {
-    setTimeout(funtion(){solveLoop(start, goal);}, 5)
+    setTimeout(function(){solveLoop(start, goal);}, 5)
   }
 }
 
