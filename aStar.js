7ab53aa24735981895c89530a2b2cd3bb72bab9c
Ytrewq13 <sam.everythingcomputers@gmail.com>
2016-11-01 09:14:36 +0000

    Update aStar.js

--- aStar.js
+++ aStar.js
@@ -10,6 +10,15 @@ function startPath(cell) {
   cell.searched = true;
   closedList.push(cell);
 }
+// Function to set the g-cost of a cell.
 function setGcost(cell) {
   cell.gCost = grid[cell.Parent].gCost + 1;
+}
+// Function to set the f-cost of a cell.
+function setFcost(cell) {
+  cell.fCost = cell.heuristic + cell.gCost;
+}
+// Function to get the Manhattan heuristic distance between 2 cells.
+function manhattanDist(cell1, goal) {
+  return (Math.abs(cell1.i - goal.i) + Math.abs(cell1.j - goal.j));
 }
