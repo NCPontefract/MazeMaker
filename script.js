59a0df9d019c07a80ea52e61277f6323b6199d74
Ytrewq13 <sam.everythingcomputers@gmail.com>
2016-11-03 08:39:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -254,7 +254,7 @@ function drawMaze() {
 }
 var grid = [];
 var built = false;
-const SIZE = 10;
+const SIZE = 80;
 var columns = Math.floor(width / SIZE);
 var rows = Math.floor(height / SIZE);
 drawMaze();
